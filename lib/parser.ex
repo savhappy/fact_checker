eef382cafb30d46ecd0911940cb4584f85ed7140
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2023-09-19 20:49:06 +0200

    query single working

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,21 +1,52 @@
 defmodule FactChecker.Parser do
   def parser(pattern) do
-    #change into $1 $2 etc...
-#if true add to list
-map_with_index = pattern
-|> Enum.uniq()
-|> Enum.filter(fn el -> is_variable?(el) end)
-|> Enum.with_index(1)
-|> Map.new
 
-modified_list = Enum.map(pattern, fn element ->
-  case Map.get(map_with_index, element) do
-    nil -> element
-    value -> :"$#{value}"
+    map_with_index =
+      pattern
+      |> Enum.uniq()
+      |> Enum.filter(fn el -> is_variable?(el) end)
+      |> Enum.with_index(1)
+      |> Map.new()
+
+    Enum.map(pattern, fn element ->
+      case Map.get(map_with_index, element) do
+        nil -> element
+        value -> :"$#{value}"
+      end
+    end)
+    |> List.to_tuple()
   end
-end)
 
-IO.inspect(modified_list)
+  def format_results(matched, pattern) when length(pattern) == 2 do
+    cond do
+      matched == [] -> write_to_file("false\n")
+      matched != [] -> write_to_file("true\n")
+    end
+  end
+
+  def format_results(result, pattern) do
+    IO.puts("not here")
+  end
+
+  def write_to_file(data) when is_list(data) do
+    file_content = Enum.join(data, " ") <> "\n"
+    file_name = "out/1.txt"
+
+    if File.exists?(file_name) do
+      File.open(file_name, [:append]) |> elem(1) |> IO.binwrite(file_content)
+    else
+      File.write(file_name, file_content)
+    end
+  end
+
+  def write_to_file(data) do
+    file_name = "out/1.txt"
+
+    if File.exists?(file_name) do
+      File.open(file_name, [:append]) |> elem(1) |> IO.binwrite(data)
+    else
+      File.write(file_name, data)
+    end
   end
 
   def is_variable?(str) do
--- lib/storage/ets.ex
+++ lib/storage/ets.ex
@@ -1,4 +1,6 @@
 defmodule FactChecker.Storage.ETS do
+
+  alias FactChecker.Parser
   def start_link() do
     IO.inspect("start")
     :ets.new(:facts, [:bag, :protected, :named_table])
@@ -16,32 +18,36 @@ defmodule FactChecker.Storage.ETS do
   def handle_query(key, values) do
     list = [key | values]
 
-    placeholder_list = Enum.map(list, fn str -> is_variable?(str) end) 
+    res = Parser.parser(list)
+
+    matched = :ets.match_object(:facts, res)
+    
+    IO.inspect(list)
+
+    Parser.format_results(matched, list)
+
+    placeholder_list = Enum.map(list, fn str -> is_variable?(str) end)
     ets_list = List.to_tuple(placeholder_list)
 
     if Enum.any?(placeholder_list, fn el -> el == :_ end) do
       query_with_placeholders(placeholder_list, values)
     else
-      query_with_no_placeholders(ets_list)
+      :ok
     end
   end
 
-  def query_with_no_placeholders(list) do
-    matched = :ets.match_object(:facts, list)
 
-    cond do
-      matched == [] -> write_to_file("false\n")
-      matched != [] -> write_to_file("true\n")
-    end
-  end
 
   def query_with_placeholders(placeholder_list, pattern) do
     [key | values] = placeholder_list
-    placeholder_list = placeholder_list
-    |> convert_to_erl()
-    |> List.to_tuple()
 
-  matched = :ets.match_object(:facts, placeholder_list) 
+    placeholder_list =
+      placeholder_list
+      |> convert_to_erl()
+      |> List.to_tuple()
+
+    matched = :ets.match_object(:facts, placeholder_list)
+
     case values do
       [:_] -> find_all(key, values) |> write_to_file
       [:_, :_] -> double_associations(matched, pattern)
@@ -59,62 +65,60 @@ defmodule FactChecker.Storage.ETS do
     end
   end
 
-
-  
-  #queries that look for values based on two placeholders
+  # queries that look for values based on two placeholders
   def double_associations(matched, pattern) do
     input_list = Enum.map(matched, fn t -> Tuple.delete_at(t, 0) |> Tuple.to_list() end)
     [p_1, p_2] = pattern
+
     if p_1 == p_2 do
       for [val_1, val_2] <- input_list, val_1 == val_2 do
         val_1
       end
       |> write_to_file
     else
-      
       for [val_1, val_2] <- input_list, val_1 != val_2 do
-        [p1, p2] = Enum.map(pattern, fn str -> String.to_atom(str) end) 
+        [p1, p2] = Enum.map(pattern, fn str -> String.to_atom(str) end)
         ["{#{p1}: #{val_1}, #{p2}: #{val_2}}"]
       end
       |> write_to_file()
     end
   end
-  
-
-
 
   def many_associations(matched, placeholders, pattern) do
-
     input_list = Enum.map(matched, fn t -> Tuple.delete_at(t, 0) |> Tuple.to_list() end)
-    placeholder_list = Enum.map(placeholders, fn t -> Tuple.delete_at(t, 0) |> Tuple.to_list() end) |> List.flatten
-    if length(placeholder_list) == 2 do
 
-      index_to_remove = Enum.find_index(placeholder_list, fn x -> x != :_ end) 
-  
+    placeholder_list =
+      Enum.map(placeholders, fn t -> Tuple.delete_at(t, 0) |> Tuple.to_list() end)
+      |> List.flatten()
+
+    if length(placeholder_list) == 2 do
+      index_to_remove = Enum.find_index(placeholder_list, fn x -> x != :_ end)
+
       Enum.map(input_list, fn sublist -> List.delete_at(sublist, index_to_remove) end)
       |> write_to_file
     else
       pattern_map =
-      Enum.map(input_list, fn sublist -> Enum.zip(pattern, sublist) end) 
-      |> Enum.map(fn sublist -> create_map(sublist) end)
-      
-      reduced_list = Enum.map(pattern_map, fn submap -> reduce_to_final(submap, pattern) end) 
+        Enum.map(input_list, fn sublist -> Enum.zip(pattern, sublist) end)
+        |> Enum.map(fn sublist -> create_map(sublist) end)
+
+      reduced_list = Enum.map(pattern_map, fn submap -> reduce_to_final(submap, pattern) end)
 
       if input_list == reduced_list do
         Enum.map(pattern_map, fn map ->
-          res = Map.to_list(map) |> format_list 
-          "{#{res}}"end) 
+          res = Map.to_list(map) |> format_list
+          "{#{res}}"
+        end)
         |> write_to_file
       else
         write_to_file("false \n")
       end
-      |> IO.inspect
+      |> IO.inspect()
     end
   end
 
   def format_list(tuple) do
     for {key, value} <- tuple do
-     "#{key}: #{value}"
+      "#{key}: #{value}"
     end
     |> Enum.join(", ")
   end
@@ -122,25 +126,26 @@ defmodule FactChecker.Storage.ETS do
   def reduce_to_final(submap, pattern) do
     Enum.map(pattern, fn p ->
       value = Map.get(submap, p)
-            case value do
-              nil -> p
-              value -> value
-            end
-          end)     
+
+      case value do
+        nil -> p
+        value -> value
+      end
+    end)
   end
 
   def create_map(list) do
     unique_key_value_pairs =
-      for {key, value} <- list 
-      |> Enum.uniq_by(fn {k, _} -> k end) 
-      |> Enum.filter( fn {k, _} -> String.match?(k, ~r/^[A-Z]/) end),
-      into: %{} do
+      for {key, value} <-
+            list
+            |> Enum.uniq_by(fn {k, _} -> k end)
+            |> Enum.filter(fn {k, _} -> String.match?(k, ~r/^[A-Z]/) end),
+          into: %{} do
         {key, value}
       end
   end
 
-
-  #write to file
+  # write to file
   def write_to_file(data) when is_list(data) do
     file_content = Enum.join(data, " ") <> "\n"
     file_name = "out/1.txt"
@@ -162,7 +167,6 @@ defmodule FactChecker.Storage.ETS do
     end
   end
 
-
   def convert_to_erl(list) do
     for item <- list do
       case item do
@@ -173,8 +177,7 @@ defmodule FactChecker.Storage.ETS do
     end
   end
 
-
-  #helpers
+  # helpers
   def is_variable?(str) do
     if String.match?(str, ~r/^[A-Z]/) do
       :_
